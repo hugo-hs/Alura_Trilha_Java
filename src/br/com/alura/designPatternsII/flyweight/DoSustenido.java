9cd224c7b95520fac46706d00c7d94c8712c2ebb
Hugo Henrique de Souza <hugo.h.s@hotmail.com>
2016-07-18 21:25:14 -0300

    fim flyweight

---
+++ src/br/com/alura/designPatternsII/flyweight/DoSustenido.java
@@ -0,0 +1,9 @@
+package br.com.alura.designPatternsII.flyweight;
+public class DoSustenido implements Nota {
+
+    @Override
+    public String simbolo() {
+        return "C#";
+    }
+
+}
--- src/br/com/alura/designPatternsII/flyweight/NotasMusicais.java
+++ src/br/com/alura/designPatternsII/flyweight/NotasMusicais.java
@@ -1,25 +1,39 @@
 package br.com.alura.designPatternsII.flyweight;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NotasMusicais {
 
     private static Map<String, Nota> notas = new HashMap<String, Nota>();
 
+    private static List<Class<? extends Nota>> clazzes;
+
     static {
-
-        notas.put("do", new Do());
-        notas.put("re", new Re());
-        notas.put("mi", new Mi());
-        notas.put("fa", new Fa());
-        notas.put("sol", new Sol());
-        notas.put("la", new La());
-        notas.put("si", new Si());
-
+         clazzes = Arrays.asList(
+                    Do.class, Re.class, Mi.class, Fa.class, 
+                    Sol.class, La.class, Si.class);
     }
     
     public Nota pega(String nome) {
-        return notas.get(nome);
+    	
+    	try{
+    		
+            if(!notas.containsKey(nome)) {
+                for(Class<? extends Nota> clazz : clazzes) {
+                    if(clazz.getSimpleName().toLowerCase().equals(nome)) {
+                        notas.put(nome, (Nota) clazz.newInstance());
+                        break;
+                    }
+                }
+            }
+    		
+            return notas.get(nome);
+    	}catch(Exception e){
+    		throw new RuntimeException();
+    	}
+    		
     }
 }
